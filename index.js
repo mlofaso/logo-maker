5270b50492cf4100db5ab2ebbc8d824abac34d3f
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-04-06 13:27:04 -0700

    finished functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 
 const questions = [
   {
@@ -24,3 +25,20 @@ const questions = [
     choices: ["Triangle", "Circle", "Square"],
   },
 ];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+  let newLogo;
+
+  if (answers.shape === "Circle") {
+    newLogo = new Circle(answers.text, answers.shapeColor, answers.textColor);
+  } else if (answers.shape === "Triangle") {
+    newLogo = new Triangle(answers.text, answers.shapeColor, answers.textColor);
+  } else {
+    newLogo = new Square(answers.text, answers.shapeColor, answers.textColor);
+  }
+  fs.writeFile("logo.svg", newLogo.render(), function (err, res) {
+    if (err) throw err;
+    console.log("SVG created!");
+  });
+});
